4bb88f73ab39639499aaa20e36bebf5b80ec82f4
unknown <henrique.mendes@LAB-DEINFO-14.deinfo.lab33>
2018-07-31 15:06:46 -0300

    Projeto 20%

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <locale.h>
 
 
 //Estrutura homogênia do admin.
 
 struct Admin{
-    int id;
+
     char usuario[10];
     char senha[10];
+
 };
 
 typedef struct Admin Admin;
@@ -19,13 +22,13 @@ typedef struct Admin Admin;
 //Estrutura homogênia do cliente.
 
 struct Cliente{
-    int id;
+
     char nome[100];
     char email[100];
     char senha[50];
     char empresa[100];
 
-}
+};
 
 typedef struct Cliente Cliente;
 
@@ -35,22 +38,57 @@ typedef struct Cliente Cliente;
 //Estrutura Homogênia de Produto
 
 
-
 struct Produto{
-    int id;
+
     char nome[100];
     char tipo[100];
+    char codBarra[13];
     double preco;
-    //TODO
-}
+    int diaFabricacao;
+    int mesFabricacao;
+    int anoFabricacao;
+
+
+};
+
+typedef struct Produto Produto;
 
 //-----------------------------------------------------------------\\
 
+Admin adm[2];
 
 
 
-int main()
-{
-    printf("Hello world!\n");
+int main(){
+
+
+
+
     return 0;
 }
+
+
+
+
+void cadastroAdmin(Admin *adm){
+    printf("----------------CADASTRO--------------------");
+    printf("\n\nDigite o login do admin:\n\n");
+    scanf("%s",&adm->usuario);
+    printf("\n\nDigite a senha do admin:\n\n");
+    scanf("%s",&adm->senha);
+    printf("\n");
+    printf("--------------------------------------------");
+}
+
+void loginAdmin(Admin *adm){
+    printf("----------------LOGIN------------------------");
+    printf("\n\nDigite o login do admin:\n\n");
+    scanf("%s",&adm->usuario);
+    printf("\n\nDigite a senha do admin:\n\n");
+    scanf("%s",&adm->senha);
+    printf("\n");
+    printf("----------------------------------------------");
+}
+
+
+void cadastro
